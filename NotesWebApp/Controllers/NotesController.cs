b5dafbe7e339dd3d68a4f399e310590367a1d121
PatrickH89 <46055190+PatrickH89@users.noreply.github.com>
2021-08-06 15:32:48 +0200

    finished crud operations - create/update/delete note, read all/one note

--- NotesWebApp/Controllers/NotesController.cs
+++ NotesWebApp/Controllers/NotesController.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 using NotesWebApp.Models;
 using System;
-using System.Collections.Generic;
 using System.Linq;
 
 namespace NotesWebApp.Controllers
@@ -9,32 +9,50 @@ namespace NotesWebApp.Controllers
     [Route("notes")]
     public class NotesController : Controller
     {
-        // instance of NotesDataContext class - dependency injection
-        private readonly NotesDataContext _db;
+        /* dependency injection
+         * ---------------------------------------------------------------------*/
+        private readonly NotesDataContext _db; // instance of NotesDataContext class - dependency injection
 
-        // injecting an instance of NotesDataContext class in constructor of NotesController
-        public NotesController(NotesDataContext db)
+        public NotesController(NotesDataContext db) // injecting an instance of NotesDataContext class in constructor of NotesController
         {
             _db = db;
         }
 
-        // get all notes
+        /* get all notes
+         * ---------------------------------------------------------------------*/
         [HttpGet("")]
         public IActionResult Index()
         {
-            Note[] posts = _db.Notices.ToArray();
-            return View(posts);
+            if (_db.Notices.Any<Note>())
+            {
+                Note[] notes = _db.Notices.ToArray();
+                return View(notes);
+            }
+            return View();
         }
 
-        // display notes create view
-        [HttpGet("create")]
+        /* get one note by id
+         * ---------------------------------------------------------------------*/
+        [HttpGet("{id}")]
+        public IActionResult Note(long id)
+        {
+            if (_db.Notices.Any(x => x.Id == id))
+            {
+                Note note = _db.Notices.FirstOrDefault(x => x.Id == id);
+                return View(note);
+            }
+            return RedirectToAction("Index", "Notes");
+        }
+
+        /* create a note
+         * ---------------------------------------------------------------------*/
+        [HttpGet("create")] // display notes create view
         public IActionResult Create()
         {
             return View();
         }
 
-        // post note-data from create-view as Note-object to CreateAction of NotesController
-        [HttpPost("create")]
+        [HttpPost("create")] // post note-data from create-view as Note-object to CreateAction of NotesController
         public IActionResult Create(Note note)
         {
             if (!ModelState.IsValid) return View();
@@ -44,6 +62,53 @@ namespace NotesWebApp.Controllers
             _db.SaveChanges(); // execute it
 
             return RedirectToAction("Index", "Notes");
+        }
+
+        /* update a note
+         * ---------------------------------------------------------------------*/
+        [HttpGet("Update")] // opens update view and fills in data by id from query
+        public IActionResult Update([FromQuery]long id)
+        {
+            Note note = _db.Notices.FirstOrDefault(x => x.Id == id);
+            return View("Update", note);
+        }
+
+        [HttpPost("UpdateNote")] // update record in db with new data of form
+        public ActionResult UpdateNote(long id, [FromForm] Note note)
+        {
+            if (ModelState.IsValid)
+            {
+                Note dbReturn = _db.Notices.FirstOrDefault(x => x.Id == id);
+                dbReturn.Id = note.Id;
+                dbReturn.Subject = note.Subject;
+                dbReturn.Notice = note.Notice;
+                dbReturn.Posted = DateTime.Now;
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View("Index");
+        }
+
+        /* delete a note
+         * ---------------------------------------------------------------------*/
+        [HttpPost] // Send id from view to controller-action
+        public IActionResult Delete(long id)
+        {
+            var success = DeleteNote(id);
+            return RedirectToAction("Index");
+        }
+
+        [HttpDelete("DeleteNote/{id}")] // delete record in sql db 
+        public ActionResult<Note> DeleteNote(long id)
+        {
+            if (_db.Notices.Any(x => x.Id == id))
+            {
+                Note note = _db.Notices.FirstOrDefault(x => x.Id == id);
+                EntityEntry<Note> dbNoteReturn = _db.Notices.Remove(note);
+                _db.SaveChanges();
+                return dbNoteReturn.Entity;
+            }
+            return View("Index");
         }
     }
 }
--- README.md
+++ README.md
@@ -1,17 +1,8 @@
 # Notes Web App
-> Here I coded my first notes web application for learning ASP.NET Core MVC.
-
-## Table of contents
-* [General info](#general-info)
-* [Technologies](#technologies)
-* [Setup](#setup)
-* [Features](#features)
-* [Status](#status)
 
 ## General info
 > Here I coded my first notes web application for learning ASP.NET Core MVC.
 > It contains all CRUD operations to create, read, update  and delete a note.
-> For more detailed information about the used technologies, read Technologies
 
 ## Technologies
 * ASP.NET CORE - version 3.1
@@ -24,9 +15,8 @@ To-do list:
 * Bootstrap 4
 * CSS3
 
-
 ## Setup
-* still under construction and not working! I will write it in here, if its downloadable for usage
+* The Notes Web App only works to a limited extent! 
 
 Just download the project und let it run in Visual Studio Community. 
 The notes web app will be started and a sql db will be created thus you can start writing your first note.
@@ -34,10 +24,11 @@ The notes web app will be started and a sql db will be created thus you can star
 Later on I will explain how to use the notes web app with MS SQL Server as a live web app on your windows desktop.
 
 ## Features
-To-do list:
 * Display all notes
-* Display one note by Id
 * Create a note
+
+To-do list:
+* Display one note by Id
 * Update a note by Id
 * Delete a note by Id
 
